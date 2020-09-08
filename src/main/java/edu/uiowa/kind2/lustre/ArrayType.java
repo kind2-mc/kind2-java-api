fcdd0557d3aa0e12ac5948414ac68b6c90033a67
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2020-09-08 10:41:23 -0500

    Javadocs for Program and ArrayType.
    Signed-off-by: Abdalrhman M Mohamed <abdoo8080@outlook.com>

--- src/main/java/edu/uiowa/kind2/lustre/ArrayType.java
+++ src/main/java/edu/uiowa/kind2/lustre/ArrayType.java
@@ -9,10 +9,20 @@ package edu.uiowa.kind2.lustre;
 
 import edu.uiowa.kind2.Assert;
 
+/**
+ * The type of an array variable.
+ */
 public class ArrayType extends Type {
   public final Type base;
   public final int size;
 
+  /**
+   * Constructor
+   *
+   * @param location location of contract in a Lustre file
+   * @param base     type of the array
+   * @param size     size of the array
+   */
   public ArrayType(Location location, Type base, int size) {
     super(location);
     Assert.isNotNull(base);
@@ -21,46 +31,18 @@ public class ArrayType extends Type {
     this.size = size;
   }
 
+  /**
+   * Constructor
+   *
+   * @param base type of the array
+   * @param size size of the array
+   */
   public ArrayType(Type base, int size) {
     this(Location.NULL, base, size);
   }
 
   @Override
   public String toString() {
-    return base + "[" + size + "]";
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((base == null) ? 0 : base.hashCode());
-    result = prime * result + size;
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (!(obj instanceof ArrayType)) {
-      return false;
-    }
-    ArrayType other = (ArrayType) obj;
-    if (base == null) {
-      if (other.base != null) {
-        return false;
-      }
-    } else if (!base.equals(other.base)) {
-      return false;
-    }
-    if (size != other.size) {
-      return false;
-    }
-    return true;
+    return base + "^" + size;
   }
 }
--- src/main/java/edu/uiowa/kind2/lustre/Program.java
+++ src/main/java/edu/uiowa/kind2/lustre/Program.java
@@ -13,6 +13,18 @@ import java.util.List;
 
 import edu.uiowa.kind2.util.Util;
 
+/**
+ * This class represents a lustre program. In Kind 2, a lustre program consists of:
+ * <ul>
+ * <li>Type definitions</li>
+ * <li>Constant definitions</li>
+ * <li>Imported functions</li>
+ * <li>Imported nodes</li>
+ * <li>Contracts</li>
+ * <li>Functions</li>
+ * <li>Nodes</li>
+ * </ul>
+ */
 public class Program extends Ast {
   public final List<TypeDef> types;
   public final List<Constant> constants;
@@ -21,8 +33,21 @@ public class Program extends Ast {
   public final List<Contract> contracts;
   public final List<Function> functions;
   public final List<Node> nodes;
-  public final String main;
+  public final String main; // Nullable
 
+  /**
+   * Constructor
+   *
+   * @param location          location of assumption in a Lustre file
+   * @param types             a collection of type definitions
+   * @param constants         a collection of constant definitions
+   * @param importedFunctions a collections of Kind 2 imported functions
+   * @param importedNodes     a collections of Kind 2 imported nodes
+   * @param contracts         a collections of Kind 2 contracts
+   * @param functions         a collections of functions
+   * @param nodes             a collections of nodes
+   * @param main              id of the main node/function
+   */
   public Program(Location location, List<TypeDef> types, List<Constant> constants,
       List<ImportedFunction> importedFunctions, List<ImportedNode> importedNodes,
       List<Contract> contracts, List<Function> functions, List<Node> nodes, String main) {
@@ -34,23 +59,15 @@ public class Program extends Ast {
     this.contracts = Util.safeList(contracts);
     this.functions = Util.safeList(functions);
     this.nodes = Util.safeList(nodes);
-    if (main == null && nodes != null && nodes.size() > 0) {
-      this.main = nodes.get(nodes.size() - 1).id;
-    } else {
-      this.main = main;
-    }
+    this.main = main;
   }
 
+  /**
+   * Constructor
+   *
+   * @param nodes an array of nodes
+   */
   public Program(Node... nodes) {
     this(Location.NULL, null, null, null, null, null, null, Arrays.asList(nodes), null);
-  }
-
-  public Node getMainNode() {
-    for (Node node : nodes) {
-      if (node.id.equals(main)) {
-        return node;
-      }
-    }
-    return null;
   }
 }
