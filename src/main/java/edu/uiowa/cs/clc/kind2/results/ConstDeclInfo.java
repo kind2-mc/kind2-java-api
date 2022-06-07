3c1667abe4536318670a0e2e85b3da661315e1ae
Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
2022-06-07 12:42:40 +0300

    Format copyright comments.
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- src/main/java/edu/uiowa/cs/clc/kind2/results/ConstDeclInfo.java
+++ src/main/java/edu/uiowa/cs/clc/kind2/results/ConstDeclInfo.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2020, Board of Trustees of the University of Iowa All rights reserved.
+ * Copyright (c) 2020, Board of Trustees of the University of Iowa
+ * All rights reserved.
  *
  * Licensed under the BSD 3-Clause License. See LICENSE in the project root for license information.
  */
