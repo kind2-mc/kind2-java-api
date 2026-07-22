d8034139f755c6bcd5c9e2ffd7ffa2f15cb97662
Josh <jbergthold@uiowa.edu>
2026-07-22 14:23:33 -0500

    Clarify ResultListener interface

--- src/main/java/edu/uiowa/cs/clc/kind2/api/ResultListener.java
+++ src/main/java/edu/uiowa/cs/clc/kind2/api/ResultListener.java
@@ -2,9 +2,9 @@ package edu.uiowa.cs.clc.kind2.api;
 
 import edu.uiowa.cs.clc.kind2.results.Result;
 
-
+@FunctionalInterface
 public interface ResultListener {
 
-    public void onUpdate(Result result);
+    void onUpdate(Result result);
     
 }
